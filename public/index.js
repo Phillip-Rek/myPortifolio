b5cb186cfa52a4c177cc3b5266c13fbf634ba18f
phillip-rek <philliprekhotho@gmail.com>
2021-02-11 16:31:53 +0200

    moved index.js to public folder

---
+++ public/index.js
@@ -0,0 +1,26 @@
+const banner = document.getElementsByClassName("banner")[0];
+
+function changeBannerBackground() {
+    if (banner.style.backgroundImage !== 'url("./public/pexels-tranmautritam-251225.jpg")')
+        banner.style.backgroundImage = 'url("./public/pexels-tranmautritam-251225.jpg")';
+    else {
+        banner.style.backgroundImage = "url('./public/pexels-tranmautritam-326503.jpg')";
+    }
+}
+
+setInterval(changeBannerBackground, 5000);
+
+const userArgentIsChrome = () => {
+    return navigator.userAgent.indexOf("Chrome") !== -1
+}
+
+function imgOrientationForChromeBrowsers() {
+    if (userArgentIsChrome()) {
+        let els = Array.from(document.getElementsByTagName("img"));
+        for (const el of els) {
+            el.className = "profile-pic rotate90";
+        }
+    }
+}
+
+imgOrientationForChromeBrowsers()
